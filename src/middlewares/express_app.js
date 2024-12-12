3dd9749a8da571e8b3517975fcb8cf749a6b8dae
jitandra100 <chauhanlearninghub@gmail.com>
2024-12-12 12:47:48 +0530

    Update express_app.js

--- src/middlewares/express_app.js
+++ src/middlewares/express_app.js
@@ -58,13 +58,13 @@ module.exports = {
             return res.send(new serviceResponse({ status: 404, errors: err.message }))
         }
     },
-    handleRateLimit: rateLimit({
-        windowMs: 1 * 60 * 1000, // 1 minutes
-        max: 50, // Limit each IP to 5 requests per `window` (here, per 1 minutes)
-        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-        handler: (req, res, next, options) => {
-            return res.send(new serviceResponse({ status: options.statusCode, errors: [{ message: options.message }] }))
-        }
-    })
+    // handleRateLimit: rateLimit({
+    //     windowMs: 1 * 60 * 1000, // 1 minutes
+    //     max: 50, // Limit each IP to 5 requests per `window` (here, per 1 minutes)
+    //     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    //     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    //     handler: (req, res, next, options) => {
+    //         return res.send(new serviceResponse({ status: options.statusCode, errors: [{ message: options.message }] }))
+    //     }
+    // })
 }
